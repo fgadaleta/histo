af72a2292e3839577a45ea1199c97727259aab15
Francesco Gadaleta <francesco@amethix.com>
2020-08-15 17:08:29 +0200

    WIP integrate noisy_float and simplify API

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,9 +10,9 @@ fn bench_adding_samples(b: &mut test::Bencher) {
     b.iter(|| {
         let mut h = histo::Histogram::with_buckets(10, None);
         for i in 0..100 {
-            h.add_float(Float{number: i as f64});
-            h.add_float(Float{number: (i*i) as f64});
-            h.add_float(Float{number: (i * i * i) as f64});
+            h.add(i as f64);
+            h.add((i*i) as f64);
+            h.add((i * i * i) as f64);
         }
         test::black_box(h);
     });
@@ -25,9 +25,9 @@ fn bench_formatting(b: &mut test::Bencher) {
     let mut h = histo::Histogram::with_buckets(10, None);
 
     for i in 0..100 {
-        h.add_float(Float{number: i as f64});
-        h.add_float(Float{number: (i*i) as f64});
-        h.add_float(Float{number: (i * i * i) as f64});
+        h.add(i as f64);
+        h.add((i*i) as f64);
+        h.add((i * i * i) as f64);
     }
 
     b.iter(|| {
